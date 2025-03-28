dc3876caf6686d0807b8a249828ac8850a55684a
Prayash Priyansu <prayashpriyansu27@gmail.com>
2025-03-28 20:45:52 +0530

    update cors

--- main.py
+++ main.py
@@ -4,19 +4,23 @@ from model import MaternalHealthRiskModel
 
 app = FastAPI()
 
-# Allow frontend (React/Vue/Next.js) to access API
+origins = [
+    "https://maternal-health-risk.vercel.app/", 
+    "http://localhost:3000"  
+]
+
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3001"],  
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-# Load and train model once
 data_path = "./Maternal Health Risk Data Set.csv"
 model = MaternalHealthRiskModel(data_path)
-model.run_analysis()  # ✅ Train the model once when FastAPI starts
+model.run_analysis()  
 
 @app.get("/")
 def read_root():
@@ -26,7 +30,7 @@ def read_root():
 def get_result(data: dict):
     print(data)
     try:
-        prediction = model.predict(data)  # ✅ Get predictions
-        return prediction  # ✅ JSON response
+        prediction = model.predict(data)  
+        return prediction  
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
